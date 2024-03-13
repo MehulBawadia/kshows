4f93bdc184cdf116f9b9f2117f629f32f817ba9e
Mehul Bawadia <mehulbawadia@gmail.com>
2024-03-13 18:18:47 +0530

    Added meta description on home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,8 @@
 
 @section('title_meta_info')
     <title>Home Page | {{ config('app.name') }}</title>
+    <meta name="description" content="A list of movies and series sorted by user votes and poularity" />
+    <link rel="canonical" href="{{ route('home') }}" />
 @endsection
 
 @section('content')
