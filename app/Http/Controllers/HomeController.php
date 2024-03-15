1e44dddb42528544d94ab7451285ac986032c36d
Mehul Bawadia <mehulbawadia@gmail.com>
2024-03-15 20:22:46 +0530

    Code formatting via Pint

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,21 +10,21 @@ class HomeController extends Controller
     /**
      * Display the home page.
      */
-    public function index() : View
+    public function index(): View
     {
         $popularMovies = Http::withToken(config('services.tmdb.token'))
-            ->get(config('services.tmdb.base_url') . '/movie/popular')
+            ->get(config('services.tmdb.base_url').'/movie/popular')
             ->json()['results'];
 
         $startDate = today()->subDays(30)->toDateString();
         $endDate = today()->toDateString();
 
         $nowPlaying = Http::withToken(config('services.tmdb.token'))
-            ->get(config('services.tmdb.base_url') . '/discover/movie?include_adult=false&sort_by=popularity.desc&release_date.gte=' . $startDate . '&release_date.lte=' . $endDate)
+            ->get(config('services.tmdb.base_url').'/discover/movie?include_adult=false&sort_by=popularity.desc&release_date.gte='.$startDate.'&release_date.lte='.$endDate)
             ->json()['results'];
 
         $genresArray = Http::withToken(config('services.tmdb.token'))
-            ->get(config('services.tmdb.base_url') . '/genre/movie/list')
+            ->get(config('services.tmdb.base_url').'/genre/movie/list')
             ->json()['genres'];
         $genres = collect($genresArray)->mapWithKeys(function ($genre) {
             return [$genre['id'] => $genre['name']];
