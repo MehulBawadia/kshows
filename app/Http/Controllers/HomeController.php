9d6114624bb1a4a532ddd075d31f371f531aa901
Mehul Bawadia <mehulbawadia@gmail.com>
2024-03-15 16:56:53 +0530

    Code formatting via Pint

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,22 +9,21 @@ class HomeController extends Controller
 {
     /**
      * Display the home page.
-     *
      */
-    public function index() : View
+    public function index(): View
     {
         $popularMovies = Http::withToken(config('services.tmdb.token'))
-            ->get(config('services.tmdb.base_url') . '/movie/popular')
+            ->get(config('services.tmdb.base_url').'/movie/popular')
             ->json()['results'];
 
         $genresArray = Http::withToken(config('services.tmdb.token'))
-            ->get(config('services.tmdb.base_url') . '/genre/movie/list')
+            ->get(config('services.tmdb.base_url').'/genre/movie/list')
             ->json()['genres'];
         $genres = collect($genresArray)->mapWithKeys(function ($genre) {
             return [$genre['id'] => $genre['name']];
         });
 
-        return view("welcome")->with([
+        return view('welcome')->with([
             'popularMovies' => $popularMovies,
             'genres' => $genres,
         ]);
