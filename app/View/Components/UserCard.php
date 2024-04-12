f9d2ec4eb411fcee8c9a0be915a1a03efb406ef1
Mehul Bawadia <mehulbawadia@gmail.com>
2024-04-12 17:03:37 +0530

    Style: Format code via Pint

--- app/View/Components/UserCard.php
+++ app/View/Components/UserCard.php
@@ -19,7 +19,7 @@ class UserCard extends Component
     /**
      * Get the view / contents that represent the component.
      */
-    public function render() : View|Closure|string
+    public function render(): View|Closure|string
     {
         return view('components.user-card');
     }
