8a35d5ff4d0c0bbcbea51bf67e18d51a1f3f4d0b
Mehul Bawadia <mehulbawadia@gmail.com>
2024-04-12 14:36:35 +0530

    Feat: Alternative movie titles
    Displaying the Movie's alternative titles in a list format

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -15,10 +15,10 @@ class MovieController extends Controller
      *
      * @param  string  $movieId
      */
-    public function show($movieId): View
+    public function show($movieId) : View
     {
         $movie = Http::withToken(config('services.tmdb.token'))
-            ->get(config('services.tmdb.base_url').'/movie/'.$movieId.'?append_to_response=credits,videos,images')
+            ->get(config('services.tmdb.base_url') . '/movie/' . $movieId . '?append_to_response=credits,alternative_titles')
             ->json();
         $movie = $this->formatMovieDetails($movie);
 
@@ -32,18 +32,19 @@ class MovieController extends Controller
      *
      * @param  array  $movie
      */
-    private function formatMovieDetails($movie): array
+    private function formatMovieDetails($movie) : array
     {
         return [
             'id' => $movie['id'],
             'title' => $movie['title'],
-            'poster_path' => 'https://image.tmdb.org/t/p/w500/'.$movie['poster_path'],
+            'poster_path' => 'https://image.tmdb.org/t/p/w500/' . $movie['poster_path'],
             'overview' => $movie['overview'],
             'release_date' => Carbon::parse($movie['release_date'])->format('l jS F, Y'),
             'vote_average' => Number::percentage($movie['vote_average'] * 10),
             'genres' => $this->getGenres($movie['genres'])->implode(', '),
             'cast' => collect($movie['credits']['cast'])->take(12),
             'crew' => collect($movie['credits']['crew'])->take(12),
+            'alternative_titles' => $this->getAlternativeTitles($movie['alternative_titles']['titles']),
         ];
     }
 
@@ -52,7 +53,7 @@ class MovieController extends Controller
      *
      * @param  array  $genres
      */
-    private function getGenres($genres): Collection
+    private function getGenres($genres) : Collection
     {
         $data = [];
         foreach ($genres as $genre) {
@@ -60,5 +61,17 @@ class MovieController extends Controller
         }
 
         return collect($data)->sort();
+    }
+
+    /**
+     * Get the alternative titles from the provided array.
+     *
+     * @param  array $titles
+     */
+    private function getAlternativeTitles($titles) : array
+    {
+        return collect($titles)->map(function ($title) {
+            return $title['title'];
+        })->toArray();
     }
 }
