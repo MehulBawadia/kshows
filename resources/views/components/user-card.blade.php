123e6e5444b4b7e3e89ec9110ac15dcaa4f4e0ca
Mehul Bawadia <mehulbawadia@gmail.com>
2024-04-12 17:02:11 +0530

    Refactor: User card in TV Shows
    Utilizing the the user card component to display the user's name and
    their played character name.

---
+++ resources/views/components/user-card.blade.php
@@ -0,0 +1,19 @@
+@if ($user['has_profile_picture'])
+    <div class="overflow-hidden rounded-md bg-white shadow-md shadow-gray-400">
+        <a href="{{ route('person.show', $user['id']) }}">
+            {{-- <img src="{{ $user['profile_picture'] }}" alt="{{ $user['name'] }}" title="{{ $user['name'] }}"
+            class="transition duration-150 ease-in-out hover:opacity-75" /> --}}
+            <img src="{{ $user['profile_picture'] }}" alt="{{ $user['name'] }}" title="{{ $user['name'] }}"
+                class="transition duration-150 ease-in-out hover:opacity-75" width="300" height="450" />
+        </a>
+
+        <div class="mt-2 px-4 py-2">
+            <a href="#"
+                class="text-base font-semibold tracking-wider text-gray-800 transition duration-200 ease-in-out hover:text-cyan-800 focus:text-cyan-800 focus:outline-none">{{ $user['name'] }}</a>
+
+            <div class="mt-1 flex items-center text-sm tracking-wider text-gray-600">
+                {{ $user['role'] }}
+            </div>
+        </div>
+    </div>
+@endif
